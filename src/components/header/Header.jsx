59c6ebf67505b6fb3a9c9f76d97ad1b236fb7784
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-03-15 12:44:29 +0100

    Added some folders

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css'
+function Header(){
+    return(
+        <div className='header'>
+            <div className='header-image'></div>
+            <div className='header-title'><h1>Bob's Bingo</h1></div>
+        </div>
+    )
+}
+export default Header
