7df095f77d620cd5ff33c4c2895d48054b5179d1
stp <steventphung6@gmail.com>
2024-03-20 14:56:35 +0100

    High and low game
    no CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { createContext, useState} from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
+import "./App.css";
+import Header from "./components/header/Header";
+import Menu from "./components/menu/Menu";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import FlappyBird from "./components/games/flappybird/FlappyBird";
+import Dashboard from "./components/dashboard/Dashboard";
+import Leaderboard from "./components/leaderboard/Leaderboard";
+import LogIn from "./components/login/index";
+import SignUp from "./components/login/signup";
+import Snake from "./components/games/snake/snake";
 
-import './App.css'
-import Header from './components/header/Header'
-import Menu from './components/menu/Menu'
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
-import FlappyBird from './components/games/flappybird/FlappyBird'
-import Dashboard from './components/dashboard/Dashboard'
-import Leaderboard from './components/leaderboard/Leaderboard'
-import LogIn from './components/login/index'
-import SignUp from './components/login/signup'
-import Snake from './components/games/snake/snake'
-
+import HigherLower from "./components/games/highlow/highlow";
 
 const AppContext = createContext();
 
 function App() {
-  
   const [loggedInUser, setLoggedInUser] = useState(null);
-  const location = useLocation()
-  const [leaderboardData, setLeaderboardData] = useState([])
+  const location = useLocation();
+  const [leaderboardData, setLeaderboardData] = useState([]);
 
   return (
     <>
+      {location.pathname === "/" && <Header />}
+      <Menu />
 
-      {location.pathname=== '/' && <Header />}
-      <Menu/>
-
-      <AppContext.Provider value={{loggedInUser, setLoggedInUser, leaderboardData, setLeaderboardData}}>
-
+      <AppContext.Provider
+        value={{
+          loggedInUser,
+          setLoggedInUser,
+          leaderboardData,
+          setLeaderboardData,
+        }}
+      >
         <Routes>
           {!loggedInUser ? (
             <Route path="/" element={<LogIn />} />
@@ -37,19 +41,15 @@ function App() {
             <Route path="/" element={<Dashboard />} />
           )}
 
-          <Route path='/signup'
-            element={<SignUp/>}/>
+          <Route path="/signup" element={<SignUp />} />
 
-          <Route path='/dracodash' 
-            element={<FlappyBird/>}/>
+          <Route path="/dracodash" element={<FlappyBird />} />
 
-          <Route path='/snake'
-            element={<Snake />} />
+          <Route path="/snake" element={<Snake />} />
 
-          <Route path='/leaderboard' element={
+          <Route path="/higherlower" element={<HigherLower />} />
 
-          <Leaderboard/>}/>
-
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </AppContext.Provider>
     </>
---
+++ src/components/games/highlow/highlow.jsx
@@ -0,0 +1,194 @@
+import { useState, useEffect, useContext, useCallback } from "react";
+import axios from "axios";
+import { AppContext } from "../../../App";
+
+function HigherLower() {
+  const [deckId, setDeckId] = useState(null);
+  const [currentCard, setCurrentCard] = useState(null);
+  const [nextCard, setNextCard] = useState(null);
+  const [score, setScore] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  const { loggedInUser } = useContext(AppContext)
+
+  const resetGame = useCallback(() => {
+    setScore(0);
+    setIsGameOver(false);
+  }, []);
+
+  console.log(score);
+
+  const updateScore = async (userId, newScore) => {
+
+    const currentScoreResponse = await axios.get(`http://localhost:4000/leaderboard/${userId}`, {
+      headers: {
+        
+        Authorization: `Bearer ${loggedInUser.token}`,
+      },
+    });
+    console.log("score: "+ newScore)
+    const currentScore = currentScoreResponse.data.data.score;
+    console.log("current: "+ currentScore)
+
+    
+    const totalNewScore = currentScore + newScore;
+    console.log("totalnew: " + totalNewScore)
+  try {
+    const response = await axios.put(`http://localhost:4000/leaderboard/${userId}`, {
+      score: totalNewScore, 
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+         Authorization: `Bearer ${loggedInUser.token}`,
+      },
+    });
+
+    console.log('Update successful:', response.data);
+    
+    
+  } catch (error) {
+    console.error('Error updating score:', error);
+   
+   
+  }
+};
+
+  useEffect(() => {
+    initializeDeck();
+  }, []);
+
+  const initializeDeck = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      setDeckId(response.data.deck_id);
+      setScore(0);
+    } catch (error) {
+      console.error("Error initializing deck:", error);
+    }
+  };
+
+  const drawCard = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+      );
+      const drawnCard = response.data.cards[0];
+      setCurrentCard(nextCard);
+      setNextCard(drawnCard);
+      console.log("iasdkas",response.data.remaining)
+      if (response.data.remaining === 0) {
+        setIsGameOver(true)
+      }
+      
+    } catch (error) {
+      console.error("Error drawing card:", error);
+    }
+  };
+
+  if(isGameOver){
+    updateScore(loggedInUser.id, score)
+    return (
+      <div className="game-over">
+        Game Over! Score: {score} <button onClick={resetGame}>Play Again</button>
+      </div>
+    );
+  }
+
+  //Have to convert some of the values to number becuase in the API it uses string on
+  const convertRankToValue = (rank) => {
+    switch (rank) {
+      case "ACE":
+        return 14;
+      case "KING":
+        return 13;
+      case "QUEEN":
+        return 12;
+      case "JACK":
+        return 11;
+      case "0":
+        return 10;
+      default:
+        return parseInt(rank);
+    }
+  };
+
+  const handleHigherGuess = () => {
+    if (nextCard && currentCard) {
+      const currentRank = convertRankToValue(currentCard.value);
+      const nextRank = convertRankToValue(nextCard.value);
+
+      if (nextRank > currentRank) {
+        const guessedScore = calculateScore(currentRank, nextRank);
+        setScore(score + guessedScore);
+        console.log("Correct guess! Next card is higher.");
+      } else {
+        const guessedScore = calculateScore(currentRank, nextRank);
+        setScore(score + guessedScore);
+        console.log("Incorrect guess! Next card is not higher.");
+      }
+    } else {
+      console.log("Cards not yet drawn");
+    }
+    drawCard();
+  };
+
+  const handleLowerGuess = () => {
+    if (nextCard && currentCard) {
+      const currentRank = convertRankToValue(currentCard.value);
+      const nextRank = convertRankToValue(nextCard.value);
+
+      if (nextRank < currentRank) {
+        const guessedScore = calculateScore(nextRank, currentRank);
+        setScore(score + guessedScore);
+        console.log("Correct guess! Next card is lower.");
+      } else {
+        const guessedScore = calculateScore(nextRank, currentRank);
+        setScore(score + guessedScore);
+        console.log("Incorrect guess! Next card is not lower.");
+      }
+    } else {
+      console.log("Cards not yet drawn");
+    }
+    drawCard();
+  };
+
+  const calculateScore = (currentRank, nextRank) => {
+    const rankDifference = Number(nextRank - currentRank);
+    return Math.max(rankDifference);
+  };
+
+  return (
+    <div>
+      <h1>Higher or Lower Card Game</h1>
+      <p>Score: {score}</p>
+      <button
+        onClick={() => {
+          initializeDeck();
+          drawCard();
+        }}
+      >
+        Start Game
+      </button>
+      {currentCard && nextCard && (
+        <div>
+          <div>
+            Current Card: <img src={currentCard.image} alt="current card" />
+          </div>
+          <div>
+            Next Card:{" "}
+            <img
+              src="https://www.deckofcardsapi.com/static/img/back.png"
+              alt="next card"
+            />
+          </div>
+          <button onClick={handleHigherGuess}>Higher</button>
+          <button onClick={handleLowerGuess}>Lower</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default HigherLower;
